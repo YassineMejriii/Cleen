b675992b3ada870b705683f06007ad2b6d5b64a3
Nourbenguirat <benguirat.23nour@gmail.com>
2026-01-31 03:12:47 +0100

    master

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
+import logo from "../assets/logo.png"
+import "../styles/navbar.css"
+
 export default function Navbar() {
   return (
     <header className="navbar">
       <div className="container navbar-inner">
-        <a href="#" className="logo-link">
-          <h1 className="logo">FreshClean</h1>
+        <a href="#" className="brand-link" aria-label="Kingdomcleen Home">
+          <img src={logo} alt="Kingdomcleen Logo" className="logo-image" />
+          
         </a>
-        <nav>
+        <nav className="navbar-links" aria-label="Main navigation">
           <a href="#">Home</a>
-          <a href="#about">À Propos</a>
-          <a href="#services">Nos services</a>
+          <a href="#about">About Us</a>
+          <a href="#services">Our Services</a>
           <a href="#contact" className="btn">
             Contact
           </a>
